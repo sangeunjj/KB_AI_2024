c126d66252cd10556ea24a5cca772367d4736a23
khee2 <21keunghee@gmail.com>
2024-08-06 16:46:25 +0900

    initial setup: 프로젝트 초기설정 자바 17 & 스프링부트 3.3.2 세팅
    - application.properties를 .gitignore에 추가

---
+++ kb-ai-backend/src/main/java/com/kbai/corporatefinance/CorporatefinanceApplication.java
@@ -0,0 +1,13 @@
+package com.kbai.corporatefinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporatefinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorporatefinanceApplication.class, args);
+	}
+
+}
---
+++ kb-ai-backend/src/test/java/com/kbai/corporatefinance/CorporatefinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbai.corporatefinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorporatefinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
