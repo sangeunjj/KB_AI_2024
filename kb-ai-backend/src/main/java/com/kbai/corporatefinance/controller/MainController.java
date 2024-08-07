e8fe4aec02df99c14e4dd46f7e9f6bfc5c6a3bdb
khee2 <21keunghee@gmail.com>
2024-08-07 16:17:02 +0900

    feat: 직접적으로 navbar와 sidebar 포함시키는 방향으로 일단 설정

--- kb-ai-backend/src/main/java/com/kbai/corporatefinance/controller/MainController.java
+++ kb-ai-backend/src/main/java/com/kbai/corporatefinance/controller/MainController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
     @GetMapping("/")
+    public String mainPage() {
+        return "chat";
+    }
+    @GetMapping("/chat")
     public String chat() {
         return "chat";
     }
