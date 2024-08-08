21aa190f31f6f9e3a3919a35b543a6c505f516e6
khee2 <21keunghee@gmail.com>
2024-08-08 14:32:05 +0900

    wip: feat: 채팅 css 수정 및 로그아웃시 홈 화면으로 리다이렉트 되도록 설정

--- kb-ai-backend/src/main/java/com/kbai/corporatefinance/controller/LoginController.java
+++ kb-ai-backend/src/main/java/com/kbai/corporatefinance/controller/LoginController.java
@@ -38,6 +38,6 @@ public class LoginController {
     @GetMapping("/logout")
     public String logout(HttpSession session) {
         session.invalidate();
-        return "redirect:/login";
+        return "redirect:/";
     }
 }
