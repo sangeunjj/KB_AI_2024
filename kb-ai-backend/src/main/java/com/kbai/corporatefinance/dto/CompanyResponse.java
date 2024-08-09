1bce3d05b93f2817e02d225a11fe0fc021396011
khee2 <21keunghee@gmail.com>
2024-08-10 01:20:05 +0900

    feat: 기업썸네일에는 기업명, 여성임원, ESG지표, 산업현황 지역, 설립일자, 대표자만 들어가도록 수정
    - db 또는 dart api 호출로 가져올 수 있는 정보로 구성됨

--- kb-ai-backend/src/main/java/com/kbai/corporatefinance/dto/CompanyResponse.java
+++ kb-ai-backend/src/main/java/com/kbai/corporatefinance/dto/CompanyResponse.java
@@ -6,19 +6,20 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class CompanyResponse {
-    private String status;
-    private String message;
-    private String corp_name; // 회사명
-    private String stock_code; // 종목코드
-    private String ceo_nm; // 대표자명
-    private String jurir_no; // 법인등록번호
-    private String bizr_no; // 사업자등록번호
+    private String corp_name; // 기업명
+    private int femaleExecutives; // 여성임원수
+    private String esg; // ESG
+    private float sentimentScore; // 긍정/부정 점수 (산업현황)
     private String adres; // 주소
-    private String hm_url; // 홈페이지 URL
-    private String ir_url; // IR 홈페이지 URL
-    private String phn_no; // 전화번호
-    private String fax_no; // 팩스번호
-    private String induty_code; // 업종코드
     private String est_dt; // 설립일자
-    private String acc_mt; // 결산월
+    private String ceo_nm; // 대표자명
 }
+
+//        1. 기업명 -> db에서
+//        5. 여성임원 -> db에서
+//        6. ESG 지표 -> db에서
+//        7. 산업현황 -> db에서 sentimentScore의 값
+//        2. 지역 -> dart api 호출해서
+//        3. 설립일자 -> dart api 호출해서
+//        4. 대표자 -> dart api 호출해서
+//        8. 대표자 신용점수 연계보기↗️ 라는 버튼 (데이터에서 가져오는게 아니라, 그냥 버튼이 있었으면 좋겠어)
