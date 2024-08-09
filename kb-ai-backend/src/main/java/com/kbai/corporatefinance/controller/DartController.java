f322fd1e15003f983a92aa63d1c5186ec5cc7e8d
khee2 <21keunghee@gmail.com>
2024-08-10 01:18:26 +0900

    del: dartController 미사용으로 인한 삭제

--- kb-ai-backend/src/main/java/com/kbai/corporatefinance/controller/DartController.java
+++ kb-ai-backend/src/main/java/com/kbai/corporatefinance/controller/DartController.java
@@ -1,33 +1,33 @@
-package com.kbai.corporatefinance.controller;
-
-import com.kbai.corporatefinance.dto.CompanyResponse;
-import com.kbai.corporatefinance.service.DartService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@Slf4j
-@RequiredArgsConstructor
-@RestController
-@RequestMapping("/api")
-public class DartController {
-
-    @Value("${dart.api.key}")
-    private String dartApiKey;
-
-    @Autowired
-    private DartService dartService;
-
-    @GetMapping(value = "/company/{corpCode}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<CompanyResponse> getCompanyByCode(@PathVariable String corpCode) {
-        CompanyResponse company = dartService.getCompanyInfo(corpCode, dartApiKey);
-        return ResponseEntity.ok(company);
-    }
-}
+//package com.kbai.corporatefinance.controller;
+//
+//import com.kbai.corporatefinance.dto.DartResponse;
+//import com.kbai.corporatefinance.service.DartService;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.http.MediaType;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@Slf4j
+//@RequiredArgsConstructor
+//@RestController
+//@RequestMapping("/api")
+//public class DartController {
+//
+//    @Value("${dart.api.key}")
+//    private String dartApiKey;
+//
+//    @Autowired
+//    private DartService dartService;
+//
+//    @GetMapping(value = "/company/{corpCode}", produces = MediaType.APPLICATION_JSON_VALUE)
+//    public ResponseEntity<DartResponse> getCompanyByCode(@PathVariable String corpCode) { // dart api에서만 가져오기
+//        DartResponse company = dartService.getCompanyInfo(corpCode, dartApiKey);
+//        return ResponseEntity.ok(company);
+//    }
+//}
